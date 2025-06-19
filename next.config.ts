4c9ddec3e93770bf7d6078ba17f746fbc045dd0f
nuswantara-multimedia <nuswantara.multimedia@gmail.com>
2025-06-19 10:36:56 +0700

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
